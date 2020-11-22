0d03ccfcdc13ab973c3408c32c357c80ea91270a
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2020-11-22 12:10:30 -0500

    Add sun and moon.

---
+++ Assets/Scripts/OrbitAround.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitAround : MonoBehaviour
+{
+    public Vector3 axis;
+    public float speed;
+    public GameObject target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(target.transform.position, axis, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SatellitalLightBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SatellitalLightBehavior : MonoBehaviour
+{
+    public Light targetLight;
+    public bool disableOnGround = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (disableOnGround && transform.position.y < 0.0f) {
+            targetLight.enabled = false;
+        } else {
+            targetLight.enabled = true;
+        }
+    }
+}
