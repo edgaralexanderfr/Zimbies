19b5e70f5e53c9976b9d37b60e40340309e441a6
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2020-11-29 22:04:24 -0500

    Character controls 2
    - Implement Unity's New Input System and bind controls.
    - Add player basic controls and character movements.
    - Add Follower script to main camera.
    - Build first alpha version.

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Controls : MonoBehaviour
+{
+    public GameObject character;
+    public InputAction move;
+    public InputAction lookX;
+    public InputAction lookY;
+
+    private int halfScreenWidth = Screen.width / 2;
+    private int halfScreenHeight = Screen.height / 2;
+
+    private CharacterController _characterController;
+    private Animator _animator;
+    private bool _moving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = character.GetComponent<CharacterController>();
+        _animator = character.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMove();
+        UpdateLook();
+    }
+
+    // Updates the player's character movement based on the move controls input
+    void UpdateMove()
+    {
+        var inputVector = move.ReadValue<Vector2>();
+        var finalVector = new Vector3();
+
+        finalVector.x = inputVector.x;
+        finalVector.z = inputVector.y;
+
+        _characterController.Move(-finalVector * Time.deltaTime * 30.0f);
+
+        // Updates the character's movement animation:
+        if (inputVector.x == 0.0f && inputVector.y == 0.0f)
+        {
+            if (_moving)
+            {
+                _moving = false;
+                _animator.Play("Idle Aiming");
+            }
+        }
+        else
+        {
+            if (!_moving)
+            {
+                _moving = true;
+                _animator.Play("Walk Aiming");
+            }
+        }
+    }
+
+    // Updates the player's character look based on the look controls input
+    void UpdateLook()
+    {
+        float mouseX = lookX.ReadValue<float>() - halfScreenWidth;
+        float mouseY = lookY.ReadValue<float>() - halfScreenHeight;
+
+        var target = new Vector3(-mouseX, 0.0f, -mouseY);
+        character.transform.LookAt(character.transform.position + target);
+    }
+
+    void OnEnable()
+    {
+        move.Enable();
+        lookX.Enable();
+        lookY.Enable();
+    }
+
+    void OnDisable()
+    {
+        move.Disable();
+        lookX.Disable();
+        lookY.Disable();
+    }
+}
---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+    public GameObject target;
+    public GameObject follower;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var newPosition = target.transform.position + offset;
+        follower.transform.position = newPosition;
+    }
+}
